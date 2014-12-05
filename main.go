e84642b474fe017d371f4e29545507407ff15f95
anikulapo <quicksilver_sunburst@hotmail.com>
2014-12-05 13:41:18 -0800

    now with command line arguments

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 // fizzybuzzy project main.go
 package main
 
+import "flag"
+import "fmt"
+
 // function version
 
 //func main() {
@@ -47,19 +50,53 @@ package main
 
 // parameterized version with support for more than just fizz and buzz
 
+//func main() {
+
+//	MIN := 0
+//	MAX := 100
+//	STEP := 1
+
+//	fizzMap := make(map[int]string)
+//	fizzMap[3] = "Fizz"
+//	fizzMap[5] = "Buzz"
+//	fizzMap[7] = "Boom"
+
+//	for i := MIN; i <= MAX; i += STEP {
+//		//tried to have this in a sep func but had trouble passing in map
+//		var r string
+//		for k, v := range fizzMap {
+//			if i%k == 0 {
+//				r += v
+//			}
+//		}
+
+//		if len(r) > 0 {
+//			println(r)
+//		} else {
+//			println(i)
+//		}
+//	}
+//}
+
+// command line arguemnts version
 func main() {
 
-	MIN := 0
-	MAX := 100
-	STEP := 1
+	MIN := flag.Int("min", 1, "min value of print range")
+	MAX := flag.Int("max", 100, "max value of print range")
+	STEP := flag.Int("step", 1, "step value for defined range")
+	flag.Parse()
+
+	if *MAX < *MIN {
+		fmt.Printf("max value %v cannot be less than min value %v\n", *MAX, *MIN)
+		return
+	}
 
 	fizzMap := make(map[int]string)
 	fizzMap[3] = "Fizz"
 	fizzMap[5] = "Buzz"
-	fizzMap[7] = "Boom"
+	//fizzMap[7] = "Boom"
 
-	for i := MIN; i <= MAX; i += STEP {
-		//tried to have this in a sep func but had trouble passing in map
+	for i := *MIN; i <= *MAX; i += *STEP {
 		var r string
 		for k, v := range fizzMap {
 			if i%k == 0 {
